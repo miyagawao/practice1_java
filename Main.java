077004b705e0d6b5d1434f44acec96f2bb2440e8
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-06-03 09:53:09 +0000

    First commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+// Javaの基本
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+    System.out.println("こんにちは、Java");
+    // 数値の17を出力
+    System.out.println(17);
+    // 5に3を足した値を出力
+    System.out.println(5 + 3);
+    // 「5+3」を文字列として出力
+    System.out.println("5 + 3");
+    // 12を3で割った値を出力
+    System.out.println(12 / 3);
+    // 3に6を掛けた値を出力
+    System.out.println(3 * 6);
+    // 8を3で割った時の余りを出力
+    System.out.println(8 % 3);
+    
+    // "こんにちは"と"世界"を連結して出力
+    System.out.println("こんにちは" + "世界");
+    // "38"と"19"を連結して出力
+    System.out.println("38" + "19");
+    // 38と19を足して出力
+    System.out.println(38 + 19);
+    
+    // int型の変数numberを定義
+    int number;
+    // 変数numberに3を代入
+    number = 3;
+    // 変数numberを出力
+    System.out.println(number);
+    // String型の変数nameを定義
+    String name;
+    // 変数nameに"Wanko"を代入
+    name = "Wanko";
+    // 変数nameを出力
+    System.out.println(name);
+    
+    int number1 = 3;
+    // int型の変数number2を定義し、7を代入
+    int number2 = 7;
+    // number1 * number2を出力
+    System.out.println(number1 * number2);
+    // 変数textに「プログラミングを勉強しよう」を代入
+    String text = "プログラミングを勉強しよう";
+    // 「Progateで」と変数textを連結して出力
+    System.out.println("Progateで" + text);
+    
+    
+  }
+}
